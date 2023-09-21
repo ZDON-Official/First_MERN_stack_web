4ff934fc9279eba716183ff9f12a737913c80b79
Zohaib <zrana1214@gmail.com>
2023-09-21 11:02:38 -0400

    bug fixing .env

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
@@ -5,12 +7,11 @@ const app = express();
 const port = 4000;
 
 // Listen for requests
-app.listen(port, () => {
-  console.log(`listening on port ${port}!!`);
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${PORT}!!`);
 });
-
 
 // Routes
 app.get("/", (request, response) => {
-    response.json({msg: 'welcome to the app!'});
+  response.json({ msg: "welcome to the app!" });
 });
