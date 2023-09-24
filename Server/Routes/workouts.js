e8bd916a240582a2cbc4aa796b0e31035441c5f9
Zohaib <zrana1214@gmail.com>
2023-09-23 21:29:46 -0400

    added a controller folder for legibility

--- Server/Routes/workouts.js
+++ Server/Routes/workouts.js
@@ -1,32 +1,20 @@
 const express = require("express");
-
-const Workout = require("../models/workoutModel.js");
+const {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ msg: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 // GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET single workouts" });
-});
+router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post("/", async (req, res) => {
-  // Handle the request
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    // send a error response back
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 
 // DELETE a new workout
 router.delete("/:id", (req, res) => {
---
+++ Server/controllers/workoutController.js
@@ -0,0 +1,53 @@
+const Workout = require("../models/workoutModel.js");
+const mongoose = require("mongoose");
+
+// get all workouts
+const getWorkouts = async (req, res) => {
+  //sort from newest to oldest
+  const workouts = await Workout.find({}).sort({ createdAT: -1 });
+
+  res.status(200).json(workouts);
+};
+
+// get a single workout
+const getWorkout = async (req, res) => {
+  // get the id of the workout
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid ID" });
+  }
+
+  // find the workout corresponding to the id
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "Workout doesn't exist" });
+  }
+
+  // send the workout corresponding to the id
+  res.status(200).json(workout);
+};
+
+// create a new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  // try to create a new workout (add to the DB)
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    // send a error response back
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a workout
+
+// update a workout
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+};
